e83a034fb8507f922d39ab0e77e160cbf08d18c4
patratsky <patratsky@gmail.com>
2018-11-09 18:44:09 +0300

    Full Pr1

---
+++ Problem1.R
@@ -0,0 +1,36 @@
+#Problem1.1
+#Problem #1.1
+R_squared <- vector(length=1000)
+RSS <- vector(length=1000)
+
+for(i in 1:1000){
+  x <- runif(1000, min=0, max=6)
+  e <- rnorm(1000, 0, x)
+  y <- 1+2*x+e
+  model <- lm(y~x)
+  R_squared[i] <- summary(model)$r.squared
+  RSS[i] <- sum(resid(model)^2)
+}
+
+hist(R_squared)
+hist(RSS)
+
+#1.2
+f_hist<-function(n, N, e_sd,...){
+  R_squared <- vector(length=N)
+  RSS <- vector(length=N)
+  
+  for(i in 1:N){
+    x <- runif(n, min=0, max=6)
+    e <- rnorm(n, 0, e_sd)
+    y <- 1+2*x+e
+    model <- lm(y~x)
+    R_squared[i] <- summary(model)$r.squared
+    RSS[i] <- sum(resid(model)^2)
+  }
+    
+  hist(R_squared,...)
+  hist(RSS,...)
+
+}
+f_hist(1000,1000,x, xlab="a")
