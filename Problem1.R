021cfd53b202e5916a7fe49b269f49b822ab0332
patratsky <patratsky@gmail.com>
2018-11-09 18:59:13 +0300

    Added comments

--- Problem1.R
+++ Problem1.R
@@ -1,23 +1,23 @@
 #Problem1.1
 #Problem #1.1
-R_squared <- vector(length=1000)
-RSS <- vector(length=1000)
+R_squared <- vector(length=1000) #creating vector to store R_2 of predefined length
+RSS <- vector(length=1000) #creating vector to store RSS of predefined length
 
-for(i in 1:1000){
-  x <- runif(1000, min=0, max=6)
-  e <- rnorm(1000, 0, x)
-  y <- 1+2*x+e
-  model <- lm(y~x)
-  R_squared[i] <- summary(model)$r.squared
-  RSS[i] <- sum(resid(model)^2)
+for(i in 1:1000){ #running loop
+  x <- runif(1000, min=0, max=6) #generating uniform x
+  e <- rnorm(1000, 0, x) #generating normal residuals with mean=0 and sd=x
+  y <- 1+2*x+e #generating y
+  model <- lm(y~x) #regressing
+  R_squared[i] <- summary(model)$r.squared #plugging value of R_2 to vector of R_2 for particular loop 
+  RSS[i] <- sum(resid(model)^2) 
 }
 
-hist(R_squared)
+hist(R_squared) #plotting hists
 hist(RSS)
 
 #1.2
-f_hist<-function(n, N, e_sd,...){
-  R_squared <- vector(length=N)
+f_hist<-function(n, N, e_sd,...){ #creating function with described parameters, "..." stands for histogram parameters
+  R_squared <- vector(length=N) #dublicating from 1.1 and expressing parameters in general form
   RSS <- vector(length=N)
   
   for(i in 1:N){
@@ -29,8 +29,8 @@ f_hist<-function(n, N, e_sd,...){
     RSS[i] <- sum(resid(model)^2)
   }
     
-  hist(R_squared,...)
+  hist(R_squared,...) #also allowing for additional parameters
   hist(RSS,...)
 
 }
-f_hist(1000,1000,x, xlab="a")
+f_hist(1000,1000,x, xlab="a") #trying the function out
