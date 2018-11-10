49780b9dd871334c507814bab40a59efc235296d
Cordn123 <emolokovskiy@gmail.com>
2018-11-11 01:32:58 +0300

    Put the local repository back on track with the remote origin. Amending commits produces a discord for some reason.

--- Problem 2.R
+++ Problem 2.R
@@ -5,15 +5,8 @@ library("readr")
 
 unzip("hw1p2.zip") #Decided to just unzip the bloody thing, sorry not sorry 
 
-<<<<<<< HEAD
 Data <- list.files("hw1p2", full.names = TRUE) %>% #Using piping to create a dataframe 
 lapply(read_csv) %>%
 bind_cols #Dataframe for a dataset created, but id column duplictes still remain, fix pending
-=======
-Data <- list.files("hw1p2", full.names = TRUE) %>%
-lapply(read_csv) %>%
-bind_cols 
-#Dataset created, but id column duplictes still remain, fix pending
->>>>>>> e737c96edd6c1314b9458d41daf31db375468c85
 
 
