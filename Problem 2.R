6c6d6ec16da1bc9bd0cfafc97ec3f9ac0f99f7a5
patratsky <patratsky@gmail.com>
2018-11-10 16:42:31 +0300

    Some example from the lecture

--- Problem 2.R
+++ Problem 2.R
@@ -4,9 +4,13 @@ library(data.table)
 library(stringr)
 
 #Define the working directory:
-working_dir<-"/Users/PatratskyAlexander/Desktop/ICEF\ 3rd\ Year/Data\ Science/hw1p2.zip"
+working_dir<-"/Users/PatratskyAlexander/Desktop/ICEF\ 3rd\ Year/Data\ Science/HA/HA1/Data-Science-HA1/hw1p2.zip"
 
 #Read the file directly from the zip:
 y<-list_of_xmls[1]
 conn<-unz(x,y)
 data_raw<-read_xml(conn,encoding = "UTF-8")
+
+#or (i in 1:8){
+#  data_x[i] <-xml_x[i](data_raw)
+#}
