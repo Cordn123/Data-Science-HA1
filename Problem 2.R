c6d887402b115f29e459318c652c857cb90335c7
Cordn123 <emolokovskiy@gmail.com>
2018-11-11 01:12:10 +0300

    Amended comments

--- Problem 2.R
+++ Problem 2.R
@@ -3,7 +3,10 @@ rm(list=ls())
 library("dplyr")
 library("readr")
 
-unzip("hw1p2.zip")
-filenames <- list.files("hw1p2")
+unzip("hw1p2.zip") #Decided to just unzip the bloody thing, sorry not sorry 
+
+Data <- list.files("hw1p2", full.names = TRUE) %>% #Using piping to create a dataframe 
+lapply(read_csv) %>%
+bind_cols #Dataframe for a dataset created, but id column duplictes still remain, fix pending
 
 
