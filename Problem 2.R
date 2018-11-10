abea8926ec7cf1da511f540f74f83fa6a921f097
Cordn123 <emolokovskiy@gmail.com>
2018-11-11 02:41:00 +0300

    Added example without dplyr, readr and piping for the sake of clarity

--- Problem 2.R
+++ Problem 2.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
-library("dplyr")
-library("readr")
+library("dplyr") #The sexy Queen of data manipulation. 
+library("readr") #Just generally better at reading files
 
 unzip("hw1p2.zip") #Decided to just unzip the bloody thing, sorry not sorry 
 
@@ -9,4 +9,13 @@ Data <- list.files("hw1p2", full.names = TRUE) %>% #Using piping to create a dat
 lapply(read_csv) %>%
 bind_cols #Dataframe for a dataset created, but id column duplictes still remain, fix pending
 
+#Without dplyr and readr the code could look like this (for example):
+
+# Filenames <- list.files("hw1p2", full.names = TRUE)
+# Data <- lapply(Filenames,function(i){
+#  read.csv(i, header=TRUE)
+# })
+# DataFrame <- do.call(cbind.data.frame, Data)
+
+#Which is extremely cunbersome and boring, so we'll stick with dplyr
 
